169bcfb9c6ee2476362d737672310f938c6ca89d
LeKhang191 <khangleit19@gmail.com>
2026-05-05 20:59:53 +0700

    Add admin dashboard with stats and new layout

---
+++ SaigonRide/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaigonRide.Data;
+using System.Linq;
+
+namespace SaigonRide.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AdminController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Dashboard()
+        {
+            ViewBag.TotalVehicles = _context.Vehicles.Count();
+            ViewBag.TotalStations = _context.Stations.Count();
+            ViewBag.AvailableBikes = _context.Vehicles.Count(v => v.Status == "Available");
+            ViewBag.TotalUsers = _context.Users.Count();
+
+            return View();
+        }
+    }
+}
