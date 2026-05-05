2b90557a8b9e3d9d520aadbedf5e4bffbf25b14b
hoangnm2206 <nguyenminhhoangg2206@gmail.com>
2026-05-05 18:56:12 +0700

    Add Vehicle entity and vehicle listing Razor Page

---
+++ SaigonRide/Models/Entities/Vehicle.cs
@@ -0,0 +1,11 @@
+﻿using SaigonRide.Models.Entities;
+
+public class Vehicle
+{
+    public int VehicleId { get; set; }
+    public string VehicleNumber { get; set; }
+    public string Type { get; set; }
+    public string Status { get; set; }
+    public int StationId { get; set; }
+    public Station? Station { get; set; }
+}
